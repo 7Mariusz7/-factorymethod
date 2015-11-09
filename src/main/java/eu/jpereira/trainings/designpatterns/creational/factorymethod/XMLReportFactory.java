6b29a9561791e160f48f1b4a14bf1569ae63555b
7Mariusz7 <p_mariusz@outlook.com>
2015-11-09 22:27:33 +0100

    Zadanie 6

---
+++ src/main/java/eu/jpereira/trainings/designpatterns/creational/factorymethod/XMLReportFactory.java
@@ -0,0 +1,19 @@
+package eu.jpereira.trainings.designpatterns.creational.factorymethod;
+
+public class XMLReportFactory implements ReportFactory {
+
+	ReportData data;
+	XMLReport xmlRaport;
+	public XMLReportFactory(ReportData data) {
+		this.data = data;
+		createReport();
+	}
+
+	@Override
+	public Report createReport() {
+		xmlRaport = new XMLReport();
+		xmlRaport.generateReport(data);
+		return xmlRaport;
+	}
+
+}
